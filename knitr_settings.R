e8e19d9074e42247f2ebde83cde0e232c3a74f4a
John Paul Helveston <john.helveston@gmail.com>
2019-08-01 09:50:29 -0400

    Commented out settings on blank pages for speed

---
+++ knitr_settings.R
@@ -0,0 +1,10 @@
+library(knitr)
+library(here)
+options(knitr.kable.NA = '')
+
+# default figure size
+knitr::opts_chunk$set(
+    fig.width  = 4.75,
+    fig.height = 4.75,
+    fig.align  = "center"
+)
