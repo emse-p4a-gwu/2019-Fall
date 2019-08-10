2991fe04072587e3e415558308a16b17ed9fc721
John Helveston <john.helveston@gmail.com>
2019-08-10 10:15:50 -0400

    Swapped order of vectors and strings

--- testing.R
+++
@@ -1,8 +0,0 @@
-# Script for testing 
-
-setwd('/Users/jhelvy/gh/EMSE-6574/2019-Fall')
-
-rmarkdown::render('00_editing/functions.Rmd')
-rmarkdown::render('P3-functions.Rmd')
-
-rmarkdown::render_site()
