2818e8fa64bab4c847171428cee6eff3e17f8c87
John Helveston <john.helveston@gmail.com>
2019-10-29 08:51:43 -0400

    Updates from class 10

--- classNotes/10-dataframes/week10notes/week10notes.R
+++ classNotes/10-dataframes/week10notes/week10notes.R
@@ -7,6 +7,9 @@ library(stringr)
 library(ggplot2)
 library(readr)
 
+install.packages('tidyverse')
+library(tidyverse)
+
 # Beatles data:
 beatles <- tibble(
     firstName   = c("John", "Paul", "Ringo", "George"),
@@ -31,6 +34,48 @@ animals_pet = tibble(
     aveLifeSpanYrs = c(10, 12)
 )
 
+# How many rows are in the animals_farm data frame?
+nrow(animals_farm)
+
+# How many columns are in the animals_pet data frame?
+ncol(animals_pet)
+
+# Create a new data frame, animals, by combining animals_farm and animals_pet.
+animals <- bind_rows(animals_farm, animals_pet)
+
+# Create a new column in animals called type and set the values to "farm" or "pet".
+animals$type <- c('farm', 'farm', 'pet', 'pet')
+
+# Change the column names of animals to title case.
+names(animals) <- str_to_title(names(animals))
+
+
+
+
+
+
+# Create a new column, playsGuitar, which is TRUE if the band member 
+# plays the guitar and FALSE otherwise.
+beatles$playsGuitar <- (beatles$instrument == 'guitar')
+
+# Select the rows for the band members who have four-letter first names.
+beatles[(str_length(beatles$firstName)) == 4,]
+beatles[c(1,2),]
+
+# Create a new column, fullName, which contains the band member's first 
+# and last name separated by a space (e.g. "John Lennon")
+beatles$fullName <- str_c(beatles$firstName, beatles$lastName, sep=' ')
+
+
+
+
+
+
+
+
+
+
+
 # External datasets:
 
 # View all dataframes in the ggplot2 library:
@@ -40,7 +85,31 @@ data(package = "ggplot2")
 pathToData <- file.path('data', 'msleep.csv')
 msleep <- read_csv(pathToData)
 
-# Preview the msleep data frame
+
+
+# Preview the msleep data frames
 head(msleep)
 tail(msleep)
 glimpse(msleep)
+
+
+
+pathToData <- file.path('data', 'wildlife_impacts.csv')
+df <- read_csv(pathToData)
+
+head(df)
+tail(df)
+glimpse(df)
+
+# How many unique airports are in the data frame?
+unique(df$airport)
+length(unique(df$airport))
+
+# What is the earliest and latest observation in the data frame?
+min(df$incident_date)
+max(df$incident_date)
+
+# What is the lowest and highest cost of any one repair in the data frame?
+min(df$cost_repairs_infl_adj)
+max(df$cost_repairs_infl_adj)
+
