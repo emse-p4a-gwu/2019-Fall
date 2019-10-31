6c67c34c86822eb3c78149e9e759665150bc410a
John Paul Helveston <john.helveston@gmail.com>
2019-10-31 09:28:46 -0400

    First complete version of L11 lecture notes

---
+++ classNotes/11-data-wrangling/week11notes/week11notes.R
@@ -0,0 +1,38 @@
+# install.packages('tidyverse')
+library(tidyverse)
+
+# Spice girls data:
+spicegirls <- tibble(
+    firstName   = c("Melanie", "Melanie", "Emma", "Geri", "Victoria"),
+    lastName    = c("Brown", "Chisholm", "Bunton", "Halliwell", "Beckham"),
+    spice       = c("Scary", "Sporty", "Baby", "Ginger", "Posh"),
+    yearOfBirth = c(1975, 1974, 1976, 1972, 1974),
+    deceased    = c(FALSE, FALSE, FALSE, FALSE, FALSE)
+)
+
+# Bands data:
+spicegirls <- spicegirls %>%
+    select(-spice) %>%
+    mutate(band = 'spicegirls')
+beatles <- tibble(
+    firstName   = c("John", "Paul", "Ringo", "George"),
+    lastName    = c("Lennon", "McCartney", "Starr", "Harrison"),
+    yearOfBirth = c(1940, 1942, 1940, 1943),
+    deceased    = c(TRUE, FALSE, FALSE, TRUE),
+    band        = 'beatles'
+)
+bands <- bind_rows(spicegirls, beatles)
+
+
+# Exporting data
+ageSummary <- bands %>%
+    mutate(age = 2019 - yearOfBirth) %>%
+    group_by(band) %>%
+    summarise(
+        mean_age = mean(age),
+        min_age = min(age),
+        max_age = max(age))
+
+# Save the result in your "data" folder:
+savePath <- file.path('data', 'ageSummary.csv')
+write_csv(ageSummary, savePath)
