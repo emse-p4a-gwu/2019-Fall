ff392abd3209de565522e5c4d577e4c579163bb9
John Paul Helveston <john.helveston@gmail.com>
2019-11-10 20:50:31 -0500

    Added class notes 12

---
+++ classNotes/12-data-visualization/knitr_setup.R
@@ -0,0 +1,11 @@
+library(knitr)
+library(here)
+options(
+    htmltools.dir.version = FALSE,
+    knitr.table.format = "html",
+    knitr.kable.NA = '')
+knitr::opts_chunk$set(
+    warning = FALSE,
+    message = FALSE,
+    fig.retina = 3)
+
---
+++ classNotes/12-data-visualization/week12notes/week12notes.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+
+birds <- read_csv(file.path('data', 'wildlife_impacts.csv'))
+bears <- read_csv(file.path('data', 'bear_killings.csv'))
