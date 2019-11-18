77296b6b9e535d59b778e5ac88662f60fcd9c197
John Paul Helveston <john.helveston@gmail.com>
2019-11-18 17:13:25 -0500

    Added quiz5.R template file

---
+++ hw_templates/quiz5.R
@@ -0,0 +1,38 @@
+# Name (Last, First): <type name here>
+
+# Load the tidyverse library
+library(tidyverse)
+
+# --------------------------------------------------------------------
+# Answer questions [70 points]
+# --------------------------------------------------------------------
+
+# 1) [15 points]
+
+
+
+# Final answer:
+
+# 2) [15 points]
+
+
+
+# Final answer:
+
+# 3) [20 points]
+
+
+
+# Final answer:
+
+# 4) [20 points]
+
+
+
+# Final answer:
+
+# --------------------------------------------------------------------
+# Plotting [30 points]
+# --------------------------------------------------------------------
+
+# Write code here to produce the plot shown on the quiz:
